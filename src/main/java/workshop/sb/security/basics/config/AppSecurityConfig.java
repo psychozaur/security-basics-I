ede07b195e39159afa086557be0b02c6e5d2a4a8
psychozaur <m.rybicki1112@gmail.com>
2019-07-16 10:25:43 +0200

    [init] - used the proper Gradle dependency now; surpassing auto-configuration with properties or config class

--- src/main/java/workshop/sb/security/basics/config/AppSecurityConfig.java
+++ src/main/java/workshop/sb/security/basics/config/AppSecurityConfig.java
@@ -1,6 +1,10 @@
 package workshop.sb.security.basics.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /*
     Implementacja klasy konfiguracyjnej dla Spring Security
@@ -27,5 +31,16 @@ import org.springframework.beans.factory.annotation.Autowired;
     https://stackoverflow.com/questions/46999940/spring-boot-passwordencoder-error
 
  */
-public class AppSecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authBuilder) throws Exception {
+        authBuilder
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}password")
+                .roles("USER");
+    }
 }
